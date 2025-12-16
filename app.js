2cc0ff853bbc947882bf3dc83a9a36dc46d58696
qd <backend1.qd@xipetech.com>
2025-12-16 18:16:32 +0530

    Initial Statement

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }))
 app.use("/", userRouter)
 app.use("/host", hostRouter)
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>This is 404 Page</h1>`)
+})
 
 module.exports = app;
